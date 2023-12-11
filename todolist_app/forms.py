9c1c598fe0d5d1e92984a314d6f5d9d7ee12c804
PiyushCET <sahoopiyush53@gmail.com>
2023-12-11 19:42:00 +0530

    Project Completion

---
+++ todolist_app/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+from todolist_app.models import TaskList
+
+class TaskForm(forms.ModelForm):
+    class Meta:
+        model = TaskList
+        fields = ['task','done']
+        
+
+        
+        
